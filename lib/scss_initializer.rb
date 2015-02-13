09c5b0c41262fb55f870d88694e098b271efc5b9
Nat Welch <nat@natwelch.com>
2015-02-13 23:15:50 +0000

    whoops

--- lib/scss_initializer.rb
+++ lib/scss_initializer.rb
@@ -4,11 +4,11 @@ module ScssInitializer
     # See http://nex-3.com/posts/88-sass-supports-rack for more details.
     require 'sass/plugin/rack'
     Sass::Plugin.options[:always_update]     = (Padrino.env == :development)
-    Sass::Plugin.options[:css_location]      = "public/css"
+    Sass::Plugin.options[:css_location]      = Padrino.root("public/css")
     Sass::Plugin.options[:full_exception]    = (Padrino.env == :development)
     Sass::Plugin.options[:never_update]      = (Padrino.env == :production)
     Sass::Plugin.options[:style]             = :compact
-    Sass::Plugin.options[:template_location] = "scss"
+    Sass::Plugin.options[:template_location] = Padrino.root("app/css")
     app.use Sass::Plugin::Rack
   end
 end
