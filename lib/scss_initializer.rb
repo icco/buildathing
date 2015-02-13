569f04f8c20703a45d038f7e14458ebc9e8bfc21
Nat Welch <nat@natwelch.com>
2015-02-13 22:38:20 +0000

    ihmm

--- lib/scss_initializer.rb
+++ lib/scss_initializer.rb
@@ -3,10 +3,10 @@ module ScssInitializer
     # Enables support for SASS template reloading in rack applications.
     # See http://nex-3.com/posts/88-sass-supports-rack for more details.
     require 'sass/plugin/rack'
-    Sass::Plugin.options[:always_update]     = (app.settings.environment == :development)
+    Sass::Plugin.options[:always_update]     = (Padrino.env == :development)
     Sass::Plugin.options[:css_location]      = "public/css"
-    Sass::Plugin.options[:full_exception]    = (app.settings.environment == :development)
-    Sass::Plugin.options[:never_update]      = (app.settings.environment == :production)
+    Sass::Plugin.options[:full_exception]    = (Padrino.env == :development)
+    Sass::Plugin.options[:never_update]      = (Padrino.env == :production)
     Sass::Plugin.options[:style]             = :compact
     Sass::Plugin.options[:template_location] = "scss"
     app.use Sass::Plugin::Rack
